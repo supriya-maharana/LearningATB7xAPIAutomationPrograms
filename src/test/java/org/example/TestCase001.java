143715f2121352d3e01aac517a0fd4d3646e8d87
supriya-maharana <supriyamaharana26@gmail.com>
2024-10-24 23:25:03 +0530

    2 files are added

--- src/test/java/org/example/TestCase001.java
+++ src/test/java/org/example/TestCase001.java
@@ -1,2 +1,7 @@
-package org.example;public class TestCase001 {
+package org.example;
+
+public class TestCase001 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
